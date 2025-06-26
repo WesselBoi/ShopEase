e726104d83eabf0c78a3a8be8dbc4e288078d0c4
WesselBoi <mathur.saksham2005@gmail.com>
2025-06-26 18:20:57 +0530

    auth back

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const productsRouter = require('./routes/products');
+const authRouter = require('./routes/auth');
 const connectToMongoDB = require('./connection');
 const {notFound, errorHandler} = require('./middlewares/errorMiddleware');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 dotenv.config();
 
 const app = express();
 
-app.use(cors("http://localhost:5173")); // Allow requests from the frontend
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true, 
+}));
 
 const PORT = process.env.PORT || 8000;
 
@@ -16,12 +21,14 @@ connectToMongoDB()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/" ,(_,res) => {
     res.send("ecommerce Server is up and running")
 })
 
 app.use("/api/products" , productsRouter);
+app.use("/api/auth" , authRouter);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { handleLogin, handleRegister } = require('../controllers/auth');
+
+const router = express.Router()
+
+router.post("/login" , handleLogin)
+
+router.post("/register" , handleRegister)
+
+module.exports = router;
