5bd0a717533a39ab8651405f32d680c12d20d534
WesselBoi <mathur.saksham2005@gmail.com>
2025-07-07 03:42:24 +0530

    Cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: "https://shopease-ealb.onrender.com",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE" , "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
     exposedHeaders: ["Set-Cookie"],
   })
