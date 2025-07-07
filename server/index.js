c234a00eb94caa8b7319c20e7ac623b1fc0109ab
Saksham Mathur <mathur.saksham2005@gmail.com>
2025-07-07 23:19:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,4 +40,4 @@ app.use("/api/orders", orderRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0' , () => console.log(`Server is running on port ${PORT}`));
